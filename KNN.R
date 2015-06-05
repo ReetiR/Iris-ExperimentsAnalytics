55a9694346e0918e1a0551e63f862c5ae684107d
ReetiR <royritu21@yahoo.co.in>
2015-06-05 12:56:58 +0530

    KNN clustering and prediction

--- KNN.R
+++ KNN.R
@@ -13,22 +13,6 @@ iris %>% ggvis(~Petal.Length, ~Petal.Width, fill = ~Species) %>% layer_points()
 #
 
 str(iris)
-#Decision Tree
-set.seed(1234)
-ind <- sample(2, nrow(iris), replace=TRUE, prob=c(0.7, 0.3))
-trainData <- iris[ind==1,]
-testData <- iris[ind==2,]
-library(party)
-formula <- Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width
-iris_ctree <- ctree(formula, data=trainData)
-# check the prediction
-table(predict(iris_ctree), trainData$Species)
-print(iris_ctree)
-plot(iris_ctree)
-plot(iris_ctree, type="simple")
-# predict on test data
-testPred <- predict(iris_ctree, newdata = testData)
-table(testPred, testData$Species)
 
 #normalize
 library(class)
